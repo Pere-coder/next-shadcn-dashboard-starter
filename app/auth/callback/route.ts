f8880f2a80b2f05395c04bcd3bb416ce844b095c
Pere-coder <godspowerpere@yahoo.com>
2024-04-02 07:57:26 +0100

    edits

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,7 +2,6 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-// import type { Database } from '@/lib/database.types'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
@@ -13,9 +12,11 @@ export async function GET(request: NextRequest) {
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
     await supabase.auth.exchangeCodeForSession(code)
   }
-  // const productionBaseUrl = 'https://next-shadcn-dashboard-starter-orpin.vercel.app/'
-  // const dashboardUrl = new URL('/dashboard', productionBaseUrl);
+  
+  // Replace 'productionBaseUrl' with your actual production base URL
+  const productionBaseUrl = 'https://next-shadcn-dashboard-starter-orpin.vercel.app'
+  const dashboardUrl = new URL('/dashboard', productionBaseUrl)
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect('https://next-shadcn-dashboard-starter-orpin.vercel.app/dashboard')
-}
+  // URL to redirect to after sign-in process completes
+  return NextResponse.redirect(dashboardUrl.href)
+} 
